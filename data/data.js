258fa586592d696f6b8dbb17ac0c6ba2b877348e
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2024-12-14 14:43:51 +0100

    changing the structure of class name list in data

--- data/data.js
+++ data/data.js
@@ -53,67 +53,67 @@ const gliderAllClassStats = [
   [-0.25, 0.25, -0.25, -0.25, 0.25, 0, 0, -0.25, 0.25, 0.25, -0.25, 0.25],
 ];
 
-export const characterAllClassName = [
-  "Bébé Peach, Bébé Daisy",
-  "Bébé Harmonie, Lemmy",
-  "Bébé Mario, Bébé Luigi, Skelerex, Mii léger",
-  "Toadette, Wendy, Marie",
-  "Koopa, Lakitu, Bowser Jr.",
-  "Toad, Maskass, Larry",
-  "Peach Chat, Fille Inkling, Villageoise, Diddy Kong",
-  "Peach, Daisy, Yoshi, Birdo, Peachette",
-  "Mario Tanuki, Garçon Inkling, Villageois",
-  "Mario, Ludwig, Mii moyen",
-  "Luigi, Iggy, Kamek",
-  "Harmonie, Roi Boo, Link, Pauline",
-  "Flora piranha*, Mario de métal, Peach d'or rose",
-  "Waluigi, Donkey Kong, Roy, Wiggler",
-  "Wario, Bowser Skelet, Mii lourd, Funky Kong",
-  "Bowser, Morton",
-];
+export const characterAllClassName = {
+  0: "Bébé Peach, Bébé Daisy",
+  1: "Bébé Harmonie, Lemmy",
+  2: "Bébé Mario, Bébé Luigi, Skelerex, Mii léger",
+  3: "Toadette, Wendy, Marie",
+  4: "Koopa, Lakitu, Bowser Jr.",
+  5: "Toad, Maskass, Larry",
+  6: "Peach Chat, Fille Inkling, Villageoise, Diddy Kong",
+  7: "Peach, Daisy, Yoshi, Birdo, Peachette",
+  8: "Mario Tanuki, Garçon Inkling, Villageois",
+  9: "Mario, Ludwig, Mii moyen",
+  10: "Luigi, Iggy, Kamek",
+  11: "Harmonie, Roi Boo, Link, Pauline",
+  12: "Flora piranha*, Mario de métal, Peach d'or rose",
+  13: "Waluigi, Donkey Kong, Roy, Wiggler",
+  14: "Wario, Bowser Skelet, Mii lourd, Funky Kong",
+  15: "Bowser, Morton",
+};
 
-export const bodyAllClassName = [
-  "300 SL Roadster(k)*, Kart Standard(k), Cybertrombe(m)",
-  "Kart rétro(k)*, Mécabécane(m), Scooter AC(m)",
-  "Proto 8(k)*, Propulsar(k)*, Missile tornade(q)",
-  "Nautomobile(k)*, Tubul R3(k)*, Malécycle(q)",
-  "Chabriolet(k)**, Météore(ms)**, Yoshimoto(ms)*, Quad nounours(q)",
-  "Mach-célère(k), Intrépide(k), Magikart(k)",
-  "Beat-bolide(k)*, GLA(k)*, Quad Standard(q)",
-  "Cavalkart(k)*, Sport GP(ms), Epervier(ms)",
-  "Paracoccinelly(k), Scootinette(m)",
-  "Autorhino(k)*, Caravéloce(k)",
-  "Sneakart(k)**, Or(k)*, Destrier de légende(ms)",
-  "W25 Flèche d'argent(k), Moto Standard(m), Flamboyante(m), Quad Wiggler(q)",
-  "Blue Falcon(k)*, Kartoon(q)",
-  "Buggy tanuki(k)*, Koopa-mobile(k), Destrier de légende 0.1(m)",
-];
+export const bodyAllClassName = {
+  16: "300 SL Roadster(k)*, Kart Standard(k), Cybertrombe(m)",
+  17: "Kart rétro(k)*, Mécabécane(m), Scooter AC(m)",
+  18: "Proto 8(k)*, Propulsar(k)*, Missile tornade(q)",
+  19: "Nautomobile(k)*, Tubul R3(k)*, Malécycle(q)",
+  20: "Chabriolet(k)**, Météore(ms)**, Yoshimoto(ms)*, Quad nounours(q)",
+  21: "Mach-célère(k), Intrépide(k), Magikart(k)",
+  22: "Beat-bolide(k)*, GLA(k)*, Quad Standard(q)",
+  23: "Cavalkart(k)*, Sport GP(ms), Epervier(ms)",
+  24: "Paracoccinelly(k), Scootinette(m)",
+  25: "Autorhino(k)*, Caravéloce(k)",
+  26: "Sneakart(k)**, Or(k)*, Destrier de légende(ms)",
+  27: "W25 Flèche d'argent(k), Moto Standard(m), Flamboyante(m), Quad Wiggler(q)",
+  28: "Blue Falcon(k)*, Kartoon(q)",
+  29: "Buggy tanuki(k)*, Koopa-mobile(k), Destrier de légende 0.1(m)",
+};
 
-export const wheelsAllClassName = [
-  "GLA*, Standard, Standard bleu",
-  "Mastodonte*, Masto-flammes*, Archéonique",
-  "Roller, Roller azur",
-  "Classique, Classique rouge, Bois",
-  "Lisse, Cyber-lisse",
-  "Métal*, Roue Or",
-  "Bouton, Feuille",
-  "Hors-piste, Hors-piste rétro, Triforce",
-  "Coussin*, Eponge",
-];
+export const wheelsAllClassName = {
+  30: "GLA*, Standard, Standard bleu",
+  31: "Mastodonte*, Masto-flammes*, Archéonique",
+  32: "Roller, Roller azur",
+  33: "Classique, Classique rouge, Bois",
+  34: "Lisse, Cyber-lisse",
+  35: "Métal*, Roue Or",
+  36: "Bouton, Feuille",
+  37: "Hors-piste, Hors-piste rétro, Triforce",
+  38: "Coussin*, Eponge",
+};
 
-export const gliderAllClassName = [
-  "Standard, Dendinaile, Aile hylienne",
-  "Aile nuages, Parachute, Aile fleurie, Aile en papier",
-  "Aile Wario, Planeur, Or, Paravoile",
-  "Ombrelle Peach, Bowser-volant, Parapente, Parapente MKTV",
-];
+export const gliderAllClassName = {
+  30: "Standard, Dendinaile, Aile hylienne",
+  31: "Aile nuages, Parachute, Aile fleurie, Aile en papier",
+  32: "Aile Wario, Planeur, Or, Paravoile",
+  33: "Ombrelle Peach, Bowser-volant, Parapente, Parapente MKTV",
+};
 
-export const elementsAllClassName = [
-  characterAllClassName,
-  bodyAllClassName,
-  wheelsAllClassName,
-  gliderAllClassName,
-];
+export const elementsAllClassName = {
+  ...characterAllClassName,
+  ...bodyAllClassName,
+  ...wheelsAllClassName,
+  ...gliderAllClassName,
+};
 
 export const statNames = [
   "Vitesse sol",
