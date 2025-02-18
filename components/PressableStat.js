da8887f029654a0682d430bfcf34877941e51ed2
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2025-02-18 10:59:08 +0100

    upgrading pressableStat

--- components/PressableStat.js
+++ components/PressableStat.js
@@ -7,10 +7,9 @@ import {
   ScrollView,
   StyleSheet,
 } from "react-native";
-import Checkbox from "expo-checkbox";
 import { modal } from "./styles/modal";
 import { button } from "./styles/button";
-import checkbox from "./styles/checkbox";
+import { Checkbox } from "react-native-paper";
 import { useTheme } from "./styles/theme";
 import { translate } from "../i18n/translations";
 import { toggleCheck } from "../utils/toggleCheck";
@@ -18,19 +17,13 @@ import { toggleCheck } from "../utils/toggleCheck";
 const PressableStat = ({ stat, setList, keepOneCondition }) => {
   const th = useTheme();
   return (
-    <Pressable
-      style={checkbox(th).container}
-      onPress={() =>
-        toggleCheck(setList, stat.name, (keepOneCondition = keepOneCondition))
-      }
-    >
-      <Checkbox
-        value={stat.checked}
-        style={checkbox(th).square}
-        color={{ true: th.primary, false: th.on_primary }}
-      />
-      <Text style={checkbox(th).text}>{translate(stat.name)}</Text>
-    </Pressable>
+    <Checkbox.Item
+      label={translate(stat.name)}
+      status={stat.checked ? "checked" : "unchecked"}
+      onPress={() => {
+        toggleCheck(setList, stat.name, (keepOneCondition = keepOneCondition));
+      }}
+    />
   );
 };
 
