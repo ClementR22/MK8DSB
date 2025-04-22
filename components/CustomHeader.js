0487e5ea8d39ecd9c75348c64192d867eb4ce02d
NoAccount1 <mouamem2edunom@gmail.com>
2025-04-23 01:18:39 +0200

    Working on header style

--- components/CustomHeader.js
+++ components/CustomHeader.js
@@ -1,33 +1,37 @@
-import { Text, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useTheme } from '@/utils/ThemeContext';
 
-const CustomHeader = ({ title }) => {
+const CustomHeader = ({title}) => {
+  const th = useTheme();
+
   return (
     <View
       style={{
-        height: 90,
-        backgroundColor: "#ffcc00",
-        justifyContent: "flex-end",
-        paddingBottom: 12,
-        paddingHorizontal: 20,
-        borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
+        backgroundColor: th.surface_container,
+        height: 64,
+        // paddingBottom: 12,
+        // paddingHorizontal: 20,
+        // borderBottomLeftRadius: 20,
+        // borderBottomRightRadius: 20,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-        flexDirection: "row",
-        alignItems: "flex-end",
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10
       }}
     >
       <MaterialCommunityIcons
         name="car-sports"
         size={24}
         color="#333"
-        style={{ marginRight: 10 }}
+        style={{marginRight: 10}}
       />
-      <Text style={{ fontSize: 22, fontWeight: "bold", color: "#333" }}>
+      <Text style={{fontSize: 22, fontWeight: 'bold', color: '#333'}}>
         {title}
       </Text>
     </View>
