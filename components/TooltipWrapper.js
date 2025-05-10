3a1f058287c5a1270d8d0262b60cdfaad97ee898
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2025-05-10 15:33:22 +0200

    tooltip self close after 2 sec

--- components/TooltipWrapper.js
+++ components/TooltipWrapper.js
@@ -3,19 +3,20 @@ import { Pressable, Text, View } from "react-native";
 import Tooltip from "react-native-walkthrough-tooltip";
 import { translate } from "@/translations/translations";
 
-const TooltipWrapper = ({
-  tooltipText,
-  style,
-  onPress,
-  placement = "top",
-  children,
-}) => {
-  const [toolTipVisible, setToolTipVisible] = useState(false);
+const TooltipWrapper = ({ tooltipText, style, onPress, placement = "top", children }) => {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  const openTooltip = () => {
+    setTooltipVisible(true);
+    setTimeout(() => {
+      setTooltipVisible(false);
+    }, 2000);
+  };
 
   return (
     <View style={style ? style : {}}>
       <Tooltip
-        isVisible={toolTipVisible}
+        isVisible={tooltipVisible}
         content={
           <View style={{ flexDirection: "row" }}>
             <View>
@@ -24,17 +25,12 @@ const TooltipWrapper = ({
           </View>
         }
         placement={placement}
-        onClose={() => setToolTipVisible(false)}
+        onClose={() => setTooltipVisible(false)}
         backgroundColor="rgba(0,0,0,0)"
         disableShadow={true}
         showChildInTooltip={false}
       >
-        <Pressable
-          onPress={onPress}
-          onLongPress={() => {
-            setToolTipVisible(true);
-          }}
-        >
+        <Pressable onPress={onPress} onLongPress={() => openTooltip()}>
           {children}
         </Pressable>
       </Tooltip>
