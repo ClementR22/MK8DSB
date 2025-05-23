6405db966c844544cf0d842b26727a6dac13e627
NoAccount1 <mouamem2edunom@gmail.com>
2025-05-24 01:08:38 +0200

    Fixing sliders's size

--- components/StatSlider.js
+++ components/StatSlider.js
@@ -2,8 +2,6 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Slider } from "@miblanchard/react-native-slider";
 import ButtonMultiStateToggle from "./ButtonMultiStateToggle";
-
-import { vw } from "@/components/styles/theme";
 import { useTheme } from "@/contexts/ThemeContext";
 import { translate } from "@/translations/translations";
 import useSetsStore from "@/stores/useSetsStore";
@@ -14,15 +12,76 @@ const StatSlider = ({ name, sliderValue, statFilterNumber }) => {
   const setStatFilterNumber = useSetsStore((state) => state.setStatFilterNumber);
   const setStatFilterNumberWithName = (newNumber) => setStatFilterNumber(name, newNumber);
 
+  const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 12,
+      paddingVertical: 8,
+      backgroundColor: theme.surface,
+      borderWidth: 2,
+      borderRadius: 12,
+      borderColor: theme.outline_variant,
+      marginBottom: 6,
+      width: "100%", // Largeur maximale de 90% de la largeur de l'écran
+    },
+    containerTop: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: 3,
+    },
+    textContainer: {
+      flexDirection: "row", // Aligne les deux textes sur une ligne
+      flex: 1, // Prend toute la largeur disponible
+    },
+    textLeft: {
+      color: theme.on_surface,
+      fontSize: 22,
+      marginLeft: 6,
+      flexShrink: 1, // Permet de réduire la largeur du texte si nécessaire
+      maxWidth: "70%", // Largeur maximale pour le texte de gauche
+      overflow: "hidden", // Cache l'excédent du texte
+    },
+    textRight: {
+      color: theme.on_surface,
+      fontSize: 22,
+      flexShrink: 0, // Le texte de droite ne rétrécit pas
+    },
+    containerBottom: {
+      width: "100%",
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+    },
+    sliderContainer: {
+      height: 20,
+      alignItems: "stretch",
+      justifyContent: "center",
+    },
+    thumb: {
+      width: 4,
+      height: 30,
+      borderRadius: 10,
+      //backgroundColor: theme.primary,
+    },
+    track: {
+      height: 8,
+      borderRadius: 4,
+      backgroundColor: theme.secondary_container,
+    },
+  });
+
   return (
-    <View style={[styles.container, { backgroundColor: theme.surface, borderColor: theme.outline_variant }]}>
+    <View style={styles.container}>
       <View style={styles.containerTop}>
         <View style={styles.textContainer}>
-          <Text numberOfLines={1} ellipsizeMode="middle" style={[styles.textLeft, { color: theme.on_surface }]}>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="middle"
+            style={styles.textLeft}
+          >
             {translate(name)}
           </Text>
 
-          <Text style={[styles.textRight, { color: theme.on_surface }]}>
+          <Text style={styles.textRight}>
             {translate(":")}
             {sliderValue}
           </Text>
@@ -44,7 +103,7 @@ const StatSlider = ({ name, sliderValue, statFilterNumber }) => {
             maximumValue={6}
             step={0.25}
             //thumbStyle={[styles.thumb, { backgroundColor: theme.primary }]}
-            trackStyle={[styles.track, { backgroundColor: theme.secondary_container }]}
+            trackStyle={styles.track}
             thumbTouchSize={{ width: 10, height: 10 }}
           />
         </View>
@@ -52,62 +111,5 @@ const StatSlider = ({ name, sliderValue, statFilterNumber }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    //backgroundColor: theme.surface,
-    borderWidth: 2,
-    borderRadius: 12,
-    //borderColor: theme.outline_variant,
-    marginBottom: 6,
-    width: 0.9 * vw, // Largeur maximale de 90% de la largeur de l'écran
-  },
-  containerTop: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 3,
-  },
-  textContainer: {
-    flexDirection: "row", // Aligne les deux textes sur une ligne
-    flex: 1, // Prend toute la largeur disponible
-  },
-  textLeft: {
-    //color: theme.on_surface,
-    fontSize: 22,
-    marginLeft: 6,
-    flexShrink: 1, // Permet de réduire la largeur du texte si nécessaire
-    maxWidth: "70%", // Largeur maximale pour le texte de gauche
-    overflow: "hidden", // Cache l'excédent du texte
-  },
-  textRight: {
-    //color: theme.on_surface,
-    fontSize: 22,
-    flexShrink: 0, // Le texte de droite ne rétrécit pas
-  },
-  containerBottom: {
-    width: "100%",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  sliderContainer: {
-    height: 20,
-    alignItems: "stretch",
-    justifyContent: "center",
-  },
-  thumb: {
-    width: 4,
-    height: 30,
-    borderRadius: 10,
-    //backgroundColor: theme.primary,
-  },
-  track: {
-    height: 8,
-    borderRadius: 4,
-    //backgroundColor: theme.secondary_container,
-  },
-});
 
 export default StatSlider;
