e1cdc68669e4b35e501e2b88cab909ee1677eef2
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2025-05-29 18:07:09 +0200

    correct the call of useCallback in ElementsView

--- components/elementsSelector/ElementsView.js
+++ components/elementsSelector/ElementsView.js
@@ -3,34 +3,27 @@ import { StyleSheet, View } from "react-native"; // Utilisez View au lieu de Fla
 import ElementChip from "./ElementChip";
 import ElementImage from "./ElementImage";
 
-// Le composant RenderElementItem n'est plus nécessaire car nous n'utilisons plus FlatList
-// const RenderElementItem = React.memo(({ item, handlePress, isGalleryMode }) => {
-//   const { id, name, image, pressed } = item;
-//   const onItemPress = useCallback(() => handlePress(item), [handlePress, item]);
+const RenderElementItem = React.memo(({ element, handlePress, isGalleryMode }) => {
+  const { id, name, image, pressed } = element;
 
-//   return !isGalleryMode ? (
-//     <ElementChip name={name} pressed={pressed} onPress={onItemPress} source={image} />
-//   ) : (
-//     <ElementImage name={name} source={image} />
-//   );
-// });
+  const onItemPress = useCallback(() => {
+    handlePress(element);
+  }, [handlePress, element]);
+
+  return !isGalleryMode ? (
+    <ElementChip key={id} name={name} pressed={pressed} onPress={onItemPress} source={image} />
+  ) : (
+    <ElementImage key={id} name={name} source={image} />
+  );
+});
 
 const ElementsView = ({ elements, handlePress, isGalleryMode }) => {
   return (
     <View style={styles.categoryContainer}>
       {/* Utilisez View */}
-      {elements.map((element) => {
-        // Utilisez .map() directement
-        const { id, name, image, pressed } = element;
-        // Mémoïsez le callback onPress pour chaque ElementChip/ElementImage
-        const onItemPress = useCallback(() => handlePress(element), [handlePress, element]);
-
-        return !isGalleryMode ? (
-          <ElementChip key={id} name={name} pressed={pressed} onPress={onItemPress} source={image} />
-        ) : (
-          <ElementImage key={id} name={name} source={image} />
-        );
-      })}
+      {elements.map((element) => (
+        <RenderElementItem key={element.id} element={element} handlePress={handlePress} isGalleryMode={isGalleryMode} />
+      ))}
     </View>
   );
 };
