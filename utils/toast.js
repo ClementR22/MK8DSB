5a29ae21c7223b4c99ea2b0d76f7b1362326aff0
NoAccount1 <mouamem2edunom@gmail.com>
2025-06-08 14:28:16 +0200

    Add parameter to `toast.js`

--- utils/toast.js
+++ utils/toast.js
@@ -1,14 +1,7 @@
 import Snackbar from "./SnackbarManager";
 
-const showToast = (text1 = "Hello", text2 = "Ceci est une alerte temporaire 👋", type = "success") => {
-  Snackbar.show("coucou");
-  //   {
-  //   type: type,
-  //   text1: text1,
-  //   text2: text2,
-  //   position: "bottom", // Ou 'bottom'
-  //   visibilityTime: 3000, // 3 secondes
-  // });
-};
+function showToast (text = "Ceci est une alerte temporaire 👋") {
+  Snackbar.show(text);
+}
 
 export default showToast;
