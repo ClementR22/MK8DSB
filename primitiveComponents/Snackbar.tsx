b176f63ccb22444f8601223375d075166d569fac
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2025-06-24 06:25:42 +0200

    SnackBar timer is reset when new click

--- primitiveComponents/Snackbar.tsx
+++ primitiveComponents/Snackbar.tsx
@@ -12,11 +12,13 @@ import { useThemeStore } from "@/stores/useThemeStore";
 type State = {
   visible: boolean;
   title?: string;
+  // on ajouter une key pour forcer le re-mount
+  key: number;
 };
 
 const Snackbar = () => {
   const theme = useThemeStore((state) => state.theme);
-  const [state, setState] = useState<State>({ visible: false });
+  const [state, setState] = useState<State>({ visible: false, key: 0 });
 
   const snackbarContentStyle = useMemo(
     () => ({
@@ -31,7 +33,14 @@ const Snackbar = () => {
   }, []);
 
   useEffect(() => {
-    const listener = (title: string) => setState({ visible: true, title });
+    const listener = (title: string) => {
+      setState((prevState) => ({
+        visible: true,
+        title,
+        // on increment la key pour forcer le re-mount
+        key: prevState.key + 1,
+      }));
+    };
 
     SnackbarManager.setListener(listener);
     return () => SnackbarManager.setListener(null);
@@ -39,6 +48,8 @@ const Snackbar = () => {
 
   return (
     <PaperSnackbar
+      key={state.key} // Use the key here
+      wrapperStyle={styles.wrapper} // Add wrapperStyle for positioning
       contentStyle={snackbarContentStyle}
       visible={state.visible}
       onDismiss={handleDismiss}
@@ -48,5 +59,15 @@ const Snackbar = () => {
     </PaperSnackbar>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: "absolute", // To make it appear above other content
+    bottom: 0, // Position at the bottom
+    left: 0,
+    right: 0,
+    zIndex: 9999, // Ensure it's on top
+  },
+});
 
 export default Snackbar;
