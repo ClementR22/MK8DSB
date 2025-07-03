52fd051f3d1330542b36cb43cdc20c6f39080aae
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2025-07-03 11:01:15 +0200

    correct offset of Menu, issue #151

--- components/setCard/SetCardMoreActionsButton.tsx
+++ components/setCard/SetCardMoreActionsButton.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useMemo } from "react";
-import { View } from "react-native";
+import { Platform } from "react-native";
 import { Menu } from "react-native-paper";
 import { useActionIconPropsList } from "@/hooks/useActionIconPropsList";
 import Icon, { IconType } from "react-native-dynamic-vector-icons";
 import ButtonIcon from "@/primitiveComponents/ButtonIcon";
 import { actionNamesList } from "./SetCard";
 import { ScreenName } from "@/contexts/ScreenContext";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface SetCardMoreActionsButtonProps {
   moreActionNamesList: actionNamesList;
@@ -19,6 +20,7 @@ const SetCardMoreActionsButton: React.FC<SetCardMoreActionsButtonProps> = ({
   situation,
 }) => {
   const [visible, setVisible] = useState(false);
+  const insets = useSafeAreaInsets(); // Get safe area insets
 
   const actionIconPropsList = useActionIconPropsList(moreActionNamesList, setCardIndex, situation);
 
@@ -27,34 +29,41 @@ const SetCardMoreActionsButton: React.FC<SetCardMoreActionsButtonProps> = ({
     onPressAction();
   };
 
+  const menuVerticalOffset = useMemo(() => {
+    let offset = 0;
+    if (Platform.OS !== "web") {
+      offset = -insets.top;
+    }
+    return offset;
+  }, [insets]);
+
   return (
-    <View>
-      <Menu
-        visible={visible}
-        onDismiss={() => setVisible(false)}
-        anchor={
-          <ButtonIcon
-            onPress={() => setVisible(true)}
-            tooltipText={"MoreActions"}
-            iconName={"more-vert"}
-            iconType={IconType.MaterialIcons}
+    <Menu
+      visible={visible}
+      onDismiss={() => setVisible(false)}
+      anchor={
+        <ButtonIcon
+          onPress={() => setVisible(true)}
+          tooltipText={"MoreActions"}
+          iconName={"more-vert"}
+          iconType={IconType.MaterialIcons}
+        />
+      }
+      anchorPosition="bottom"
+      style={{ marginTop: menuVerticalOffset }}
+    >
+      {actionIconPropsList.map((actionProps) => {
+        const { title, name, type, onPress } = actionProps;
+        return (
+          <Menu.Item
+            key={name}
+            onPress={() => handleMenuItemPress(onPress)}
+            title={title}
+            leadingIcon={({ color, size }) => <Icon type={type} name={name} size={size} color={color || "black"} />}
           />
-        }
-        anchorPosition="bottom"
-      >
-        {actionIconPropsList.map((actionProps) => {
-          const { title, name, type, onPress } = actionProps;
-          return (
-            <Menu.Item
-              key={name}
-              onPress={() => handleMenuItemPress(onPress)}
-              title={title}
-              leadingIcon={({ color, size }) => <Icon type={type} name={name} size={size} color={color || "black"} />}
-            />
-          );
-        })}
-      </Menu>
-    </View>
+        );
+      })}
+    </Menu>
   );
 };
 
