2bd2262233ac5f25d689064afddf219af9e15494
RAYER Clement <clement.rayer3@etu.univ-lorraine.fr>
2025-10-26 00:03:12 +0200

    remove react-native-localize which is not supported on expo go

--- translations/index.ts
+++ translations/index.ts
@@ -1,5 +1,5 @@
 import i18n from "i18next";
-import * as Localization from "react-native-localize";
+import * as Localization from "expo-localization";
 import { initReactI18next } from "react-i18next";
 import { resources } from "./resources";
 
