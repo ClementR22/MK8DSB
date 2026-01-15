427387c1f509b526da848baad6ddb496b79f1917
ClementR22 <clemv78@gmail.com>
2026-01-15 17:44:23 +0100

    upgrade WelcomeModal content

--- components/modal/WelcomeModal.tsx
+++ components/modal/WelcomeModal.tsx
@@ -1,12 +1,22 @@
-// WelcomeModal.tsx
+// components/modal/WelcomeModal.tsx
 import React, { useEffect, useState } from "react";
 import Modal from "@/primitiveComponents/Modal";
 import Text from "@/primitiveComponents/Text";
 import useGeneralStore from "@/stores/useGeneralStore";
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { useTranslation } from "react-i18next";
+import Button from "@/primitiveComponents/Button";
+import StatGaugeContainer from "../statGauge/StatGaugeContainer";
+import StatGaugeBar from "../statGauge/StatGaugeBar";
+import { useGameData } from "@/hooks/useGameData";
+import { BUTTON_SIZE } from "@/utils/designTokens";
+import Tooltip from "../Tooltip";
+import { elementsNamespaceByGame, statsNamespaceByGame } from "@/translations/namespaces";
+import useGameStore from "@/stores/useGameStore";
 
 const WelcomeModal = () => {
+  const game = useGameStore((state) => state.game);
+  const { elementsData, statNames, statNamesCompact } = useGameData();
   const { ready } = useTranslation();
   const isWelcome = useGeneralStore((state) => state.isWelcome);
   const welcomeMessage = useGeneralStore((state) => state.welcomeMessage);
@@ -21,16 +31,35 @@ const WelcomeModal = () => {
   }
 
   return (
-    <Modal
-      modalTitle="welcome"
-      isModalVisible={isModalVisible}
-      setIsModalVisible={setIsModalVisible}
-      onClose={hideWelcome}
-    >
+    <Modal modalTitle="welcome" isModalVisible={true} setIsModalVisible={setIsModalVisible} onClose={hideWelcome}>
       <View style={styles.container}>
         <Text role="body" size="large" textAlign="center" namespace="text">
           {welcomeMessage || "welcomeText"}
         </Text>
+
+        <Text role="body" size="large" textAlign="center" namespace="text">
+          longPress
+        </Text>
+      </View>
+
+      <View style={styles.row}>
+        <Button tooltipText="thisIsAnExample">button</Button>
+
+        <Tooltip tooltipText={elementsData[0].name} namespace={elementsNamespaceByGame[game]}>
+          <Image source={elementsData[0].imageUrl} style={styles.image} resizeMode="contain" />
+        </Tooltip>
+
+        <Tooltip tooltipText={statNames[0]} namespace={statsNamespaceByGame[game]}>
+          <Text
+            role="label"
+            size="large"
+            style={styles.abbreviation}
+            namespace={statsNamespaceByGame[game]}
+            textAlign="center"
+          >
+            {statNamesCompact[statNames[0]]}
+          </Text>
+        </Tooltip>
       </View>
     </Modal>
   );
@@ -38,6 +67,12 @@ const WelcomeModal = () => {
 
 const styles = StyleSheet.create({
   container: { gap: 20, padding: 10, alignItems: "center" },
+  row: { flexDirection: "row", width: "100%", justifyContent: "space-evenly", alignItems: "center" },
+  image: {
+    width: BUTTON_SIZE,
+    height: BUTTON_SIZE * 0.8,
+  },
+  abbreviation: { width: 45, marginLeft: 3, marginRight: 8 },
 });
 
 export default WelcomeModal;
